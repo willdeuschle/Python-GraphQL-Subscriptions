1c9cdb2f8952275223b853401fc7bd8c9392f462
willdeuschle <wjdeuschle@gmail.com>
2018-01-02 13:33:21 -0500

    last push

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 from setuptools import setup
 
+required_packages = ['pyee', 'graphql-core']
+
 setup(name='python_graphql_subscriptions',
-      version='0.1.2',
-      description='SubscriptionManager for GraphQL with python backend',
+      version='0.1.5',
+      description='Adds support for subscriptions to GraphQL applications that use a Python backend.',
       url='https://github.com/willdeuschle/Python-GraphQL-Subscriptions',
       author='Will Deuschle',
       author_email='wjdeuschle@gmail.com',
       license='MIT',
       packages=['python_graphql_subscriptions'],
-      install_requires=[
-          'pyee',
-          'graphql-core',
-      ],
-      setup_requires=[
-          'pytest-runner',
-      ],
-      tests_require=[
-          'pytest',
-      ],
+      install_requires=required_packages,
+      tests_require=['pytest'],
       zip_safe=False)
